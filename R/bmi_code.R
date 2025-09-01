da6473518d9b3a931112968779c74284a17f1f0f
Alejandra H. Segura <43819685+AleSR13@users.noreply.github.com>
2025-09-01 15:56:41 +0200

    [CHORE] Update renv and R code structure

--- R/bmi_code.R
+++ R/bmi_code.R
@@ -3,9 +3,11 @@
 #' @param height Height in meters
 #' @param weight Weight in kilograms
 #'
-#' @returns numeric value for the BMI
+#' @return numeric value for the BMI
+#' @export
 #'
-#' @examples calculate_bmi(height = 1.70, weight = 65)
+#' @examples
+#' calculate_bmi(height = 1.70, weight = 65)
 calculate_bmi <- function(height, weight) {
   if (height <= 0) stop("Height must be greater than zero.")
   if (weight <= 0) stop("Weight must be greater than zero.")
@@ -14,13 +16,13 @@ calculate_bmi <- function(height, weight) {
 }
 
 
+
 #' Calculates BMI in a data frame
 #'
 #' @param df Data frame with at least two columns: height and weight
 #'
-#' @returns Data frame (same as input) with an extra column for the 'bmi'
-#'
-#' @examples calculate_bmi_df(df = input_df)
+#' @return Data frame (same as input) with an extra column for the 'bmi'
+#' @export
 calculate_bmi_df <- function(df) {
   if (!all(c("height", "weight") %in% names(df))) {
     stop("Dataframe must contain 'height' and 'weight' columns.")
