fe71c6c69fd5a2e51b42a84133483f9237895d77
Alejandra Hernandez Segura | HUB Organoids <a.segura@huborganoids.nl>
2025-09-01 13:09:26 +0200

    [CHORE] Re-factoring to use approved folder structure

--- R/styler_demo.R
+++ R/styler_demo.R
@@ -10,27 +10,31 @@ my_helper();
 
 
 # Spacing between parenthesis and after a comma
-another_variable = mean(
-seq( 1,10,1 ),234,24,3534,132,45,65463,454,231,123,11,1133,na.rm=TRUE )
-
-# Long-function calls
-do_something_very_complicated("that", requires, many, arguments,
-                              "some of which may be long"
+another_variable <- mean(
+seq( 1,10,1 ),234,24,3534,132,45,65463,454,231,123,11,1133,
+na.rm=TRUE
 )
+# Long-function calls
+do_something_very_complicated(
+                              "that", requires, many, arguments,
+                              "some of which may be long"
+                              )
 
 # Named arguments with a space
 my_function(
+  second_function(
     x=x,
     y=long_argument_name,
     extra_argument_a=10,
     extra_argument_b=c(1, 43, 390, 210209)
+  )
 )
 
 # Multi-line statement with no braced expressions
 if (x > 10)
-    x * 2
+  x * 2
 
 # Pipes not corrected by styler
 strings %>%
-    str_replace("a", "b") |>
-    str_replace("x", "y")
+  str_replace("a", "b") |>
+  str_replace("x", "y")
